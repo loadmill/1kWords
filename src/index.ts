9af4498ef569749c2bff18da8d126dbd717189dd
idoco <ido@loadmill.com>
2022-03-17 20:28:38 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-// import * as core from '@actions/core';
 import * as github from '@actions/github';
 
 function run() {
-  // const name: string = core.getInput('my_input');
-  // if (name) {
-  //   core.debug(`Hello ${name}!`);
-  //   return core.setOutput('my_output', `Hello ${name}!`);
-  // }
-  // core.setFailed('my_input not specified!');
 
+  console.log(`action starting`);
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
 
