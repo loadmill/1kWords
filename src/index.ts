153617757edada870e2c6769cbadad376a63f915
idoco <ido@loadmill.com>
2022-03-17 18:37:49 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import * as core from '@actions/core';
+// import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 function run() {
-  const name: string = core.getInput('my_input');
-  if (name) {
-    core.debug(`Hello ${name}!`);
-    return core.setOutput('my_output', `Hello ${name}!`);
-  }
-  core.setFailed('my_input not specified!');
+  // const name: string = core.getInput('my_input');
+  // if (name) {
+  //   core.debug(`Hello ${name}!`);
+  //   return core.setOutput('my_output', `Hello ${name}!`);
+  // }
+  // core.setFailed('my_input not specified!');
+
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
 }
 
 run();
