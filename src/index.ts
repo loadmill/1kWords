d952bfae99b8b280cc0b3560c002bc11da0473fc
Ido Cohen <ido.c.mail@gmail.com>
2022-03-20 11:19:36 +0200

    Print the current PR title in quotes

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ function run() {
 
   const pullRequestTitle = github.context.payload.pull_request?.title || "";
 
-  console.log(`The PR title is ${pullRequestTitle}`);
+  console.log(`The PR title is "${pullRequestTitle}"`);
 
   // Regular expression to match strings starting with emoji
   const regexExp = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]).*/gi;
